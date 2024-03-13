f85aee9b30f8a4567820481d6f0e6d74e765f5b5
funkybunny2 <yangchee828@gmail.com>
2024-03-12 19:52:31 -0500

    did some documenting

--- leetcode/238-product-of-array-except-self.py
+++ leetcode/238-product-of-array-except-self.py
@@ -8,15 +8,21 @@ class Solution(object):
         ans = []  # Initialize ans as an empty list
         
         # Calculate left products
+        # for nums = [1,2,3,4]
+        # ans would look like this ans = [1,1,2,6]
         left_product = 1
         for i in range(n):
             ans.append(left_product)
             left_product *= nums[i]
         
         # Calculate right products and multiply with existing ans
+        # for nums = [1,2,3,4]
+        # ans would look like this ans = [24,12,4,1]
         right_product = 1
         for i in range(n-1, -1, -1):
             ans[i] *= right_product
             right_product *= nums[i]
+        
+        #mutiplying both together you get the answer: ans =[24,12,8,6]
         
         return ans
